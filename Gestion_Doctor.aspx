309e976b2b6b9aca5ce9af81f50df42cf9113c45
JUAN JOSE VELEZ CAYOLA <jvelezc@edu.uc.ac.cr>
2025-08-23 20:24:38 -0600

    Diseño de pagina para gestionar los datos de los doctores

---
+++ Gestion_Doctor.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Gestion_Doctor.aspx.vb" Inherits="Sistema_de_Control_de_Citas_Médicas.Gestion_Doctor" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:SqlDataSource ID="SqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:Conexion %>"
+      SelectCommand="SELECT * FROM [Tabla_Doctor]">
+    </asp:SqlDataSource>
+
+     <h3>Formulario</h3>
+
+    <div class="form-group mb-3">
+         <label for="TxtNombreDc">NombreDc</label>
+         <asp:TextBox ID="TxtNombreDc" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+
+     <div class="form-group mb-3">
+          <label for="TxtApellidosDc">ApellidosDc</label>
+          <asp:TextBox ID="TxtApellidosDc" runat="server" CssClass="form-control"></asp:TextBox>
+     </div>
+
+     <div class="form-group mb-3">
+         <label for="TxtEdadDc">EdadDc</label>
+         <asp:TextBox TextMode="Number" ID="TxtEdadDc" runat="server" CssClass="form-control"></asp:TextBox>
+     </div>
+
+     <div class="form-group mb-3">
+        <label for="TxtTelefonoDc">TelefonoDc</label>
+        <asp:TextBox ID="TxtTelefonoDc" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+
+      <div class="form-group mb-3">
+           <label for="TxtDireccionDc">DireccionDc</label>
+           <asp:TextBox ID="TxtDireccionDc" runat="server" CssClass="form-control"></asp:TextBox>
+      </div>
+
+     <div class="form-group mb-3">
+       <label for="TxtCorreoDc">CorreoDc</label>
+       <asp:TextBox ID="TxtCorreoDc" runat="server" CssClass="form-control"></asp:TextBox>
+     </div>
+
+      <div class="form-group mb-3">
+         <label for="TxtEspecialidad">Especialidad</label>
+         <asp:TextBox ID="TxtEspecialidad" runat="server" CssClass="form-control"></asp:TextBox>
+      </div>
+
+      <div class="form-group mb-3">
+         <label for="TxtExperiencia">Experiencia</label>
+         <asp:TextBox ID="TxtExperiencia" runat="server" CssClass="form-control"></asp:TextBox>
+      </div>
+
+      
+
+
+     <div class="form-group">
+         <asp:Button ID="btnGuardar" CssClass="btn btn-primary" runat="server" Text="Guardar" OnClick="btnGuardar_Click" />
+     </div>
+
+     <div class="form-group">
+         <asp:Button ID="btnCancelar" CssClass="btn btn-primary" runat="server" Text="Limpiar" OnClick="btnCancelar_Click" />
+     </div>
+
+     <div class="form-group mb-3">
+         <asp:Label ID="LblMensaje" runat="server" Text=""></asp:Label>
+     </div>
+
+</asp:Content>
---
+++ Gestion_Doctor.aspx.designer.vb
@@ -0,0 +1,123 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Gestion_Doctor
+
+    '''<summary>
+    '''Control SqlDataSource.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource As Global.System.Web.UI.WebControls.SqlDataSource
+
+    '''<summary>
+    '''Control TxtNombreDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtNombreDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtApellidosDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtApellidosDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtEdadDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtEdadDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtTelefonoDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtTelefonoDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtDireccionDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtDireccionDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtCorreoDc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtCorreoDc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtEspecialidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtEspecialidad As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TxtExperiencia.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtExperiencia As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control btnGuardar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnGuardar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnCancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCancelar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control LblMensaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LblMensaje As Global.System.Web.UI.WebControls.Label
+End Class
